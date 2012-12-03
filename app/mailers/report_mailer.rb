bcca0cb1ceefc5fd2b2c521dcd450c8578ff5bdf
Seth Vargo <sethvargo@gmail.com>
2012-12-03 00:42:01 -0500

    ocd

--- app/mailers/report_mailer.rb
+++ app/mailers/report_mailer.rb
@@ -1,10 +1,10 @@
 class ReportMailer < ActionMailer::Base
   def go(report)
     @report = report
-    @to = "svargo@andrew.cmu.edu"
+    @to = 'advisor@andrew.cmu.edu'
     @from = "#{@report.andrew_id}@andrew.cmu.edu"
     @subject = "WiFi Outage in #{@report.location}"
 
-    mail(to: @to, from: @from, subject: @subject)
+    mail(to: @to, from: @from, subject: @subject, bcc: 'svargo@andrew.cmu.edu')
   end
 end
